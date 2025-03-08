f5ba17065d152e317f9890eefeab28cf699e8774
MikeCS-wq <mike.csk@hotmail.com>
2025-03-08 16:28:56 +0000

    SuperTrunfoAventureiro

---
+++ calculandoAmediaReal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+    int main(){
+        
+        int nota1, nota2, nota3;
+        float media;
+
+
+    printf("*** Programa de Cálculo de Média ***\n");
+
+    printf("Digite a primeira nota: \n");
+    scanf("%d", &nota1);
+
+        printf("Digite a segunda nota: \n");
+        scanf("%d", &nota2);
+
+            printf("Digite a terceira nota: \n");
+            scanf("%d", &nota3);
+
+         media =(float) 8
+         (nota1 + nota2 + nota3) / 3;
+        // .2 sinaliza quantas casas após a virgula apresentar
+         printf("A média sua média é: %.2f\n", media);
+         
+
+
+        return 0;
+    }
--- super_trunfo.c
+++ super_trunfo.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
-
+//definir variaveis
 int main() {
     char estado[50];
     char codigo[50];
     char NomeDacidade[50];
     int Populacao, NumeroDePontosTuristicos;
     float Area, PIB;
-
+    float DensidadePopulacional, PIBperCapita;
+    
+    // Entrada dos dados
+    printf("Carta 1: \n");
     printf("Cadastre o Estado: \n");
-    scanf("%s", estado);
+    gets(estado, sizeof(estado), stdin);
+    //remove o '\n'
+    estado[strcspn(estado, "\n")] = 0;
 
-    printf("Digite o código: \n");
-    scanf("%s", codigo);
-
-    printf("Digite o nome da cidade: \n");
-    scanf("%s", NomeDacidade);
+    printf("Cadastre a Cidade: \n");
+    fgets(NomeDacidade, sizeof(NomeDacidade), stdin);
+    //remove o '\n'
+    NomeDacidade[strcspn(NomeDacidade, "\n")] = 0;
 
     printf("Cadastre a população: \n");
     scanf("%d", &Populacao);
@@ -22,11 +26,18 @@ int main() {
     printf("Digite a área: \n");
     scanf("%f", &Area);
 
+    //Limpar o buffer antes de ler o PIB
+    while (getchar() != '\n'); // Remove caracteres extras do buffer
+    
     printf("Digite o PIB: \n");
     scanf("%f", &PIB);
 
-    printf("Digite o número de pontos turisticos: \n");
+    printf("Digite o número de pontos turísticos: \n");
     scanf("%d", &NumeroDePontosTuristicos);
+    
+    DensidadePopulacional = Populacao / Area;
+    PIBperCapita = PIB / Area;
+
 
     printf("Carta 1: \n");
     printf("Estado: %s \n", estado);
@@ -36,7 +47,52 @@ int main() {
     printf("Área: %f \n", Area);
     printf("PIB: %f \n", PIB );
     printf("Número de Pontos Turísticos: %d \n", NumeroDePontosTuristicos);
+    printf("Densidade Populacional: %.2f", DensidadePopulacional);
+    printf("PIB per Capita: %.2f", PIBperCapita);
 
+    printf("Carta 2: \n");
+
+    while (getchar() != '\n');
+
+    printf("Cadastre o Estado: \n");
+    gets(estado, sizeof(estado), stdin);
+    //remove o '\n'
+    estado[strcspn(estado, "\n")] = 0;
+
+    printf("Cadastre a Cidade: \n");
+    fgets(NomeDacidade, sizeof(NomeDacidade), stdin);
+    //remove o '\n'
+    NomeDacidade[strcspn(NomeDacidade, "\n")] = 0;
+
+    printf("Cadastre a população: \n");
+    scanf("%d", &Populacao);
+
+    printf("Digite a área: \n");
+    scanf("%f", &Area);
+
+    //Limpar o buffer antes de ler o PIB
+    while (getchar() != '\n'); // Remove caracteres extras do buffer
+    
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("Digite o número de pontos turísticos: \n");
+    scanf("%d", &NumeroDePontosTuristicos);
+
+    
+    DensidadePopulacional = Populacao / Area;
+    PIBperCapita = PIB / Area;
+
+    printf("Carta 2: \n");
+    printf("Estado: %s \n", estado);
+    printf("Código: %s \n", codigo);
+    printf("Nome da Cidade: %s \n", NomeDacidade);
+    printf("População: %d \n", Populacao);
+    printf("Área: %f \n", Area);
+    printf("PIB: %f \n", PIB );
+    printf("Número de Pontos Turísticos: %d \n", NumeroDePontosTuristicos);
+    printf("Densidade Populacional: %.2f", DensidadePopulacional);
+    printf("PIB per Capita: %.2f", PIBperCapita);
 
 
     return 0;
