c9e02730cc62fb41a91aab735d6455823c08f027
MikeCS-wq <mike.csk@hotmail.com>
2025-03-08 14:15:35 +0000

    atualizeiOcode

---
+++ OperadoresMat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+    int main(){
+        
+        int nota1, nota2, nota3;
+        int media;
+
+
+    printf("*** Programa de Cálculo de Média ***\n");
+
+    printf("Digite a primeira nota: \n");
+    scanf("%d". &nota1);
+
+        printf("Digite a segunda nota: \n");
+        scanf("%d". &nota2);
+
+            printf("Digite a terceira nota: \n");
+            scanf("%d". &nota3);
+
+         media = (nota1 + nota2 + nota3) / 3;
+
+         printf("A média sua média é: %d\n", media);
+         
+
+
+        return 0;
+    }
