0fe2084fe3c5e4091042f22b02d7157c8328a6e9
MikeCS-wq <mike.csk@hotmail.com>
2025-03-15 17:06:44 +0000

    Atualização Nivel Mestre

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,98 +1,100 @@
 #include <stdio.h>
 //definir variaveis
 int main() {
-    char estado[50];
-    char codigo[50];
-    char NomeDacidade[50];
-    int Populacao, NumeroDePontosTuristicos;
-    float Area, PIB;
-    float DensidadePopulacional, PIBperCapita;
-    
+
+    char estado1[50], estado2[50];
+    char codigo1[50], codigo2[50];
+    char NomeDacidade1[50], NomeDacidade2[50];
+    unsigned long int Populacao1, Populacao2;
+    int NumeroDePontosTuristicos1, NumeroDePontosTuristicos2;
+    float Area1, Area2, PIB1, PIB2;
+    float DensidadePopulacional1, DensidadePopulacional2, PIBperCapita1, PIBperCapita2;
+    float SuperPoder1, SuperPoder2;
+
     // Entrada dos dados
     printf("Carta 1: \n");
+
+    printf("Cadastre o código: \n");
+    scanf("%s", codigo1);
+
     printf("Cadastre o Estado: \n");
-    gets(estado, sizeof(estado), stdin);
-    //remove o '\n'
-    estado[strcspn(estado, "\n")] = 0;
+    scanf("%s", estado1);
 
     printf("Cadastre a Cidade: \n");
-    fgets(NomeDacidade, sizeof(NomeDacidade), stdin);
-    //remove o '\n'
-    NomeDacidade[strcspn(NomeDacidade, "\n")] = 0;
+    scanf("%s", NomeDacidade1);
 
     printf("Cadastre a população: \n");
-    scanf("%d", &Populacao);
+    scanf("%lu", &Populacao1);
 
     printf("Digite a área: \n");
-    scanf("%f", &Area);
-
-    //Limpar o buffer antes de ler o PIB
-    while (getchar() != '\n'); // Remove caracteres extras do buffer
+    scanf("%f", &Area1);
     
     printf("Digite o PIB: \n");
-    scanf("%f", &PIB);
+    scanf("%f", &PIB1);
 
     printf("Digite o número de pontos turísticos: \n");
-    scanf("%d", &NumeroDePontosTuristicos);
+    scanf("%d", &NumeroDePontosTuristicos1);
     
-    DensidadePopulacional = Populacao / Area;
-    PIBperCapita = PIB / Area;
+    //PROXIMA CARTA/////////////////////////////////
+    printf("Carta 2: \n");
 
+    printf("Cadastre o código: \n");
+    scanf("%s", codigo2);
+
+    printf("Cadastre o Estado: \n");
+    scanf("%s", estado2);
+
+    printf("Cadastre a Cidade: \n");
+    scanf("%s", NomeDacidade2);
+
+    printf("Cadastre a população: \n");
+    scanf("%lu", &Populacao2);
+
+    printf("Digite a área: \n");
+    scanf("%f", &Area2);
+    
+    printf("Digite o PIB: \n");
+    scanf("%f", &PIB2);
+
+    printf("Digite o número de pontos turísticos: \n");
+    scanf("%d", &NumeroDePontosTuristicos2);
+    
+    //CALCULO DE DP E PIBPERCAPITA
+
+    DensidadePopulacional1 = Populacao1 / Area1;
+    PIBperCapita1 = PIB1 / Area1;
+
+    DensidadePopulacional2 = Populacao2 / Area2;
+    PIBperCapita2 = PIB2 / Area2;
+
+    SuperPoder1 = Populacao1 + Area1 + PIB1 + NumeroDePontosTuristicos1 + PIBperCapita1 + DensidadePopulacional1;
+    SuperPoder2 = Populacao2 + Area2 + PIB2 + NumeroDePontosTuristicos2 + PIBperCapita2 + DensidadePopulacional2;
 
     printf("Carta 1: \n");
-    printf("Estado: %s \n", estado);
-    printf("Código: %s \n", codigo);
-    printf("Nome da Cidade: %s \n", NomeDacidade);
-    printf("População: %d \n", Populacao);
-    printf("Área: %f \n", Area);
-    printf("PIB: %f \n", PIB );
-    printf("Número de Pontos Turísticos: %d \n", NumeroDePontosTuristicos);
-    printf("Densidade Populacional: %.2f", DensidadePopulacional);
-    printf("PIB per Capita: %.2f", PIBperCapita);
+    printf("Código: %s \n", codigo1);
+    printf("Super Poder: %5.f \n", SuperPoder1);
+    printf("Estado: %s \n", estado1);
+    printf("Nome da Cidade: %s \n", NomeDacidade1);
+    printf("População: %lu \n", Populacao1);
+    printf("Área: %f \n", Area1);
+    printf("PIB: %f \n", PIB1 );
+    printf("Número de Pontos Turísticos: %d \n", NumeroDePontosTuristicos1);
+    printf("Densidade Populacional: %.2f \n", DensidadePopulacional1);
+    printf("PIB per Capita: %.2f \n", PIBperCapita1);
 
     printf("Carta 2: \n");
+    printf("Código: %s \n", codigo2);
+    printf("Super Poder: %5.f \n", SuperPoder1);
+    printf("Estado: %s \n", estado2);
+    printf("Nome da Cidade: %s \n", NomeDacidade2);
+    printf("População: %lu \n", Populacao2);
+    printf("Área: %f \n", Area2);
+    printf("PIB: %f \n", PIB2 );
+    printf("Número de Pontos Turísticos: %d \n", NumeroDePontosTuristicos2);
+    printf("Densidade Populacional: %.2f \n", DensidadePopulacional2);
+    printf("PIB per Capita: %.2f \n", PIBperCapita2);
 
-    while (getchar() != '\n');
 
-    printf("Cadastre o Estado: \n");
-    gets(estado, sizeof(estado), stdin);
-    //remove o '\n'
-    estado[strcspn(estado, "\n")] = 0;
-
-    printf("Cadastre a Cidade: \n");
-    fgets(NomeDacidade, sizeof(NomeDacidade), stdin);
-    //remove o '\n'
-    NomeDacidade[strcspn(NomeDacidade, "\n")] = 0;
-
-    printf("Cadastre a população: \n");
-    scanf("%d", &Populacao);
-
-    printf("Digite a área: \n");
-    scanf("%f", &Area);
-
-    //Limpar o buffer antes de ler o PIB
-    while (getchar() != '\n'); // Remove caracteres extras do buffer
-    
-    printf("Digite o PIB: \n");
-    scanf("%f", &PIB);
-
-    printf("Digite o número de pontos turísticos: \n");
-    scanf("%d", &NumeroDePontosTuristicos);
-
-    
-    DensidadePopulacional = Populacao / Area;
-    PIBperCapita = PIB / Area;
-
-    printf("Carta 2: \n");
-    printf("Estado: %s \n", estado);
-    printf("Código: %s \n", codigo);
-    printf("Nome da Cidade: %s \n", NomeDacidade);
-    printf("População: %d \n", Populacao);
-    printf("Área: %f \n", Area);
-    printf("PIB: %f \n", PIB );
-    printf("Número de Pontos Turísticos: %d \n", NumeroDePontosTuristicos);
-    printf("Densidade Populacional: %.2f", DensidadePopulacional);
-    printf("PIB per Capita: %.2f", PIBperCapita);
 
 
     return 0;
